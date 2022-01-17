f1b0336a6da9d022b9c2a88f6aa1fe5dafc24c0f
BLANCA DOMINGO <blaaanca@yahoo.es>
2022-01-18 00:14:15 +0100

    ahora si que gute nacht...

--- src/components/MainNews.jsx
+++ src/components/MainNews.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./MainNews.css";
 import ItemNews from "./ItemNews";
 import { useEffect, useState } from "react";
+import * as FaIcons from "react-icons/fa";
 
 function MainNews({ apiParam, searchText }) {
   const [news, setNews] = useState("");
   let newsApi =
     "https://newsapi.org/v2/top-headlines?country=de&apiKey=1ac2aebda1c64e04a5bd4828db18f788";
   let title = "Deutschland";
+  let icon = <FaIcons.FaFlag />
 
   switch (apiParam) {
     case "de":
@@ -83,7 +85,7 @@ function MainNews({ apiParam, searchText }) {
     <div className="main-news">
       <div>
         <div className="title">
-        <h2>{news && title}</h2>
+        <h2>{news && title}</h2> {/* <h3>{news && icon}</h3> */}
 
          {/*  <iframe
             className="weather"
